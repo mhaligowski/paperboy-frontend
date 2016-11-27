cc4b3d0c4e5df8f565d3e85a8451ba5d78daaf23
Mateusz Haligowski <mhaligowski@gmail.com>
2016-11-27 00:16:06 -0800

    Nuke the Rails app, move towards React

--- app/controllers/sessions_controller.rb
+++
@@ -1,7 +0,0 @@
-# class SessionsController < ApplicationController
-#
-#   def after_sign_in_path_for(resource)
-#     stored_location_for(resource) || redirect_url
-#   end
-#
-# end
--- app/models/admin.rb
+++
@@ -1,6 +0,0 @@
-class Admin < Patron
-
-  def decrement_invitation_limit!
-    #stub functionality because devise invitation call this method to decrement the counter.
-  end
-end
