6a5b26e05bac490629a3b41683c7b24d30d7a1fc
Mateusz Haligowski <mhaligowski@gmail.com>
2016-11-28 21:17:31 -0800

    Initialize panels for displaying titles

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import React, { Component } from 'react';
-import { Navbar } from 'react-bootstrap';
+import { Navbar, Grid, Accordion, Panel, Row, Col } from 'react-bootstrap';
 
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Navbar staticTop>
-        <Navbar.Header>
-          <Navbar.Brand>
-            <a href="#">Paperboy</a>
-          </Navbar.Brand>
-          <Navbar.Toggle />
-        </Navbar.Header>
-      </Navbar>
+      <div>
+        <Navbar staticTop>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#">Paperboy</a>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+        </Navbar>
+
+        <Grid>
+          <Row>
+            <Col md={8} mdOffset={2} xs={12}>
+              <Accordion accordion>
+                <Panel header="First title">
+                </Panel>
+                <Panel header="Second title">
+                </Panel>
+              </Accordion>
+            </Col>
+          </Row>
+        </Grid>
+      </div>
     );
   }
 }
