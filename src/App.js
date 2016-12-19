5f492199e755359c80f8dace4cf63e0b7abf93f3
Mateusz Haligowski <mhaligowski@gmail.com>
2016-12-18 17:43:05 -0800

    Add icons to the navigation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { Navbar } from 'react-bootstrap';
+import { Navbar, Nav, NavItem, Glyphicon } from 'react-bootstrap';
+
+import { IndexLinkContainer } from 'react-router-bootstrap';
 
 import './App.css';
 
@@ -14,9 +16,26 @@ class App extends Component {
             </Navbar.Brand>
             <Navbar.Toggle />
           </Navbar.Header>
+
+
+          <Navbar.Collapse>
+            <Nav pullRight>
+              <IndexLinkContainer to="/">
+                <NavItem eventKey={1}>
+                  <Glyphicon glyph="list-alt"/>
+                </NavItem>
+              </IndexLinkContainer>
+              <IndexLinkContainer to="/subscriptions">
+                <NavItem eventKey={2}>
+                  <Glyphicon glyph="eye-open" bsSize="large"/>
+                </NavItem>
+              </IndexLinkContainer>
+            </Nav>
+          </Navbar.Collapse>
         </Navbar>
 
-        { this.props.children }
+
+        {this.props.children}
       </div>
     );
   }
